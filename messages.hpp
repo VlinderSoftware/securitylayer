fd9f98541f37bf884f4d1e8c80fc875c77501d50
Ronald Landheer-Cieslak <ronald.landheer-cieslak@ieee.org>
2019-06-30 13:33:14 -0400

    formatting and comment correction

--- messages.hpp
+++ messages.hpp
@@ -12,13 +12,13 @@ static_assert(DNP3SAV6_PROFILE_HPP_INCLUDED, "profile.hpp should be pre-included
 namespace DNP3SAv6 {
 	enum struct Message : std::uint8_t
 	{
-	  request_session_initiation__	= 0x01
-	, session_start_request__	= 0x02
-	, session_start_response__	= 0x03
-	, set_keys__			= 0x04
-	, key_status__			= 0x05
-	, authenticated_apdu__		= 0x06
-	, error__			= 0x20 // must be 0x10 to be able to mimic IIN2.5
+		  request_session_initiation__	= 0x01
+		, session_start_request__		= 0x02
+		, session_start_response__		= 0x03
+		, set_keys__					= 0x04
+		, key_status__					= 0x05
+		, authenticated_apdu__			= 0x06
+		, error__						= 0x20 // must be 0x20 to be able to mimic IIN2.5
 	};
 }
 
