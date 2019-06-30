18bd1db79ca13f437468cdb2a6139b83986e1895
Ronald Landheer-Cieslak <ronald.landheer-cieslak@ieee.org>
2019-06-30 13:29:57 -0400

    Align MAC algorithms to WG15 working document for 62351-5

--- macalgorithm.hpp
+++ macalgorithm.hpp
@@ -8,13 +8,12 @@ static_assert(DNP3SAV6_PROFILE_HPP_INCLUDED, "profile.hpp should be pre-included
 namespace DNP3SAv6 {
 	enum struct MACAlgorithm : std::uint8_t
 	{
-		  hmac_sha_256_truncated_8__ = 3
-		, hmac_sha_256_truncated_16__ = 4
-		, hmac_sha_256__ = 7
-		, hmac_blake2s_truncated_8__ = 8
-		, hmac_blake2s_truncated_16__ = 9
-		, hmac_blake2s__ = 10
-		//TODO add SHA-3-based algorithms
+		  hmac_sha_256_truncated_8__	= 3
+		, hmac_sha_256_truncated_16__	= 4
+		, hmac_sha_3_256_truncated_8__	= 7
+		, hmac_sha_3_256_truncated_16__ = 8
+		, hmac_blake2s_truncated_8__	= 9
+		, hmac_blake2s_truncated_16__	= 10
 	};
 }
 
