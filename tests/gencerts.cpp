82876f079e5edfbf9e41ca0abde2bd1948625cb1
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2020-03-01 12:30:52 -0500

    Implement load, store, etc.

--- tests/gencerts.cpp
+++ tests/gencerts.cpp
@@ -1,5 +1,6 @@
 #include "../details/certificate.hpp"
 
+using namespace std;
 using namespace DNP3SAv6;
 
 int main()
@@ -7,6 +8,7 @@ int main()
     using Details::Certificate;
 
     Certificate certificate(Certificate::generate("/CN=Device name", 30, "prime256v1", "sha256"));
+    certificate.store(string("test.crt"), string("Crazy"));
 
     return 0;
 }
