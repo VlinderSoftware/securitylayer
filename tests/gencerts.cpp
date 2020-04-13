35db19eac7c401fd1e92d8c91957e7b2222b7f50
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2020-04-13 14:12:58 -0400

    POC: certificate formats

--- tests/gencerts.cpp
+++ tests/gencerts.cpp
@@ -7,7 +7,7 @@ int main()
 {
     using Details::Certificate;
 
-    Certificate certificate(Certificate::generate("/CN=Device name", 30, "prime256v1", "sha256"));
+    Certificate certificate(Certificate::generate("/CN=Device name", Certificate::makeOptions(7305/*twenty years*/, "prime256v1", "sha256")));
     certificate.store(string("test.crt"), string("Crazy"));
 
     return 0;
