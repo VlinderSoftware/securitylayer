0daef0efe08af2649b164affcbdd30845a7806cd
Ronald Landheer-Cieslak <rlc@vlinder.ca>
2022-06-05 16:48:38 -0400

    fix comment

--- keywrapalgorithm.hpp
+++ keywrapalgorithm.hpp
@@ -22,7 +22,7 @@ namespace DNP3SAv6 {
 	enum struct KeyWrapAlgorithm : std::uint8_t
 	{
           unknown__                 = 0 // not a valid over-the-wire value
-		// key wrap as defined by NIST SP800-38F using AES-256 in GCM mode.
+		// key wrap as defined by NIST SP800-38F using AES-256.
 		, rfc3394_aes256_key_wrap__ = 2
 	};
 }
